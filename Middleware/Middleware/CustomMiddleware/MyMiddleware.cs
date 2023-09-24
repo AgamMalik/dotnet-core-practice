b5d52a731f9ec9eb1aa5b3ab09b8555f8be73bee
AgamMalik <agam.malik31@gmail.com>
2023-09-24 17:06:55 +0530

    next

---
+++ Middleware/Middleware/CustomMiddleware/MyMiddleware.cs
@@ -0,0 +1,10 @@
+﻿namespace Middleware.CustomMiddleware
+{
+    public class MyMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
